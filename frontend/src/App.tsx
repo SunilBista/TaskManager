e1a14a8854cc6ca46b622f1f3f526d7965e69077
SunilBista <sunilbista303@gmail.com>
2025-06-02 18:14:23 +1000

    feat: setup the App component for different page routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,28 @@
-import { useState } from "react";
-import "./App.css";
-import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
+import Dashboard from "./components/Dashboard";
 
-const App: React.FC = () => {
-  const [count, setCount] = useState<number>(0);
+function App() {
   return (
-    <>
-      <p>Current count is {count}</p>
-      <button onClick={() => setCount(count + 1)}>Increase</button>
-    </>
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+      <div className="w-full">
+        <Router>
+          <Routes>
+            <Route path="/" element={<Navigate to="/login" />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+          </Routes>
+        </Router>
+      </div>
+    </div>
   );
-};
+}
 
 export default App;
