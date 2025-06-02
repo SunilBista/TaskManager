4132a598623d771d3db295eb6952a4132a8f872c
SunilBista <sunilbista303@gmail.com>
2025-06-02 18:15:36 +1000

    feat: add util for data format

---
+++ frontend/src/components/utils/formatDate.tsx
@@ -0,0 +1,24 @@
+export const formatDate = (dateString: string) => {
+  const date = new Date(dateString);
+  const day = date.getDate();
+
+  const getOrdinal = (n: number) => {
+    if (n > 3 && n < 21) return "th";
+    switch (n % 10) {
+      case 1:
+        return "st";
+      case 2:
+        return "nd";
+      case 3:
+        return "rd";
+      default:
+        return "th";
+    }
+  };
+
+  const ordinal = getOrdinal(day);
+  const month = date.toLocaleString("default", { month: "long" });
+  const year = date.getFullYear();
+
+  return `${day}${ordinal} ${month}, ${year}`;
+};
